3380a9678be8acc629cdc69fcc717cb9d261fbc0
vadymshturkhal <vadymshturkhal@gmail.com>
2020-06-06 09:22:09 +0300

    Solved all tasks.

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,25 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => {
+  const [firstArg, secondArg, result] = types;
+
+  const inner = f => (...args) => {
+    const checkValues = [];
+
+    checkValues.push(firstArg(`${args[0]}`));
+    checkValues.push(secondArg(`${args[1]}`));
+    const funcResult = f(...args);
+    checkValues.push(result(`${funcResult}`));
+
+    args.push(funcResult);
+
+    for (let i = 0; i < 3; i++) {
+      const isEqualTypes = checkValues[i] === args[i];
+      if (!isEqualTypes) throw new TypeError();
+    }
+    return funcResult;
+  };
+  return inner(fn);
+};
 
 module.exports = { contract };
